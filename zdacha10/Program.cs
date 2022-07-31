ff5c481326546a22e43c1266bdc1b3edb22ac500
Chusta <i.chusta18@mail.ru>
2022-07-31 19:00:39 +0300

    Задача10

---
+++ zdacha10/Program.cs
@@ -0,0 +1,7 @@
+﻿
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите цифру");
+int a = Convert.ToInt32(Console.ReadLine( ));
+
+Console.WriteLine(a>99 ? a.ToString()[1] : "");
+Console.ReadKey(true);
