fe0cefe0009b88923a7149897d9bf4a170e1207f
Chusta <i.chusta18@mail.ru>
2022-08-02 19:25:40 +0300

    Задача15

---
+++ zadacha15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("введите число ");
+int a = Convert.ToInt32(Console.ReadLine( ));
+
+if (a == 6)
+{
+    Console.WriteLine("ДА");
+    
+
+}
+if (a == 7)
+{
+    Console.WriteLine("ДА");
+    
+
+} 
+  
